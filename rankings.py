b6bf8d17c62c3112cd6042c2d7163e8dcda31173
sgsmob <sgsmob@gmail.com>
2019-12-19 20:28:30 -0500

    MACHINE LEARNING

--- rankings.py
+++ rankings.py
@@ -1,21 +1,12 @@
-import networkx as nx
 import sys
 import csv
 import datetime
+import random
+import os
+import networkx as nx
+from sklearn.naive_bayes import GaussianNB
 
-class Bowl:
-    def __init__(self, name, away, home):
-        self.name = name
-        self.away = away
-        self.home = home
-        
-    def score_diff(self):
-        return self.away.combined_scores() - self.home.combined_scores()
-        
-    def __str__(self):
-        return self.name
-
-        
+       
 class Team:
     def __init__(self, name, win_score, loss_score):
         self.name = name
@@ -56,11 +47,13 @@ def date_from_string(date_string):
     return datetime.date(int(year), month_map[month], int(day))
 
 
-def sanity_check(G):
+def prune(G):
+    removal = []
     for v in G.nodes():
         if G.in_degree(v) + G.out_degree(v) < 10:
-            raise ValueError("{} should have won more than {} or lost more than {} games".format(v, G.in_degree(v), G.out_degree(v)))
-
+            removal.append(v)
+            # raise ValueError("{} should have won more than {} or lost more than {} games".format(v, G.in_degree(v), G.out_degree(v)))
+    G.remove_nodes_from(removal)
 
 # Results from https://www.sports-reference.com/cfb/.
 def load_results_from_file(filename):
@@ -80,33 +73,65 @@ def load_results_from_file(filename):
             else:
                 G.add_edge(game.loser, game.winner)
     # sanity_check(G)
+    prune(G)
     return G, bowls
  
  
 def scored_teams(G):
     win_scores = nx.pagerank_numpy(G, alpha=0.9)
     loss_scores = nx.pagerank_numpy(G.reverse(), alpha=0.9)
-    return {team: Team(team, win_score, loss_scores[team]) for team, win_score in win_scores.iteritems()}
+    return {team: Team(team, win_score, loss_scores[team]) for team, win_score in win_scores.items()}
     
 
-def decide(bowls, teams):
+def decide(bowls, teams, model):
     for bowl in bowls:
-        score = teams[bowl.winner].combined_scores() - teams[bowl.loser].combined_scores()
-        if score > 0:
-            yield score, bowl.name, bowl.winner
+        #score = teams[bowl.winner].combined_scores() - teams[bowl.loser].combined_scores()
+        w_team = teams[bowl.winner]
+        l_team = teams[bowl.loser]
+        score = model.predict_prob([w_team.win_score, w_team.loss_score, l_team.win_score, l_team.loss_score])
+        if score[1] > score[0]:
+            yield score[1], bowl.name, bowl.winner
         else:
-            yield -1*score, bowl.name, bowl.loser
-            
+            yield score[0], bowl.name, bowl.loser
+
+
+def train(teams_by_year, bowls_by_year):
+    training_data = []
+    training_labels = []
+    for year in sorted(bowls_by_year.keys())[:-1]:
+        current_bowls = bowls_by_year[year]
+        current_teams = teams_by_year[year]
+        for bowl in current_bowls:
+            wws = current_teams[bowl.winner].win_score
+            wls = current_teams[bowl.winner].loss_score
+            lws = current_teams[bowl.loser].win_score
+            lls = current_teams[bowl.loser].loss_score
+            if random.randint(0, 1) == 1:
+                training_data.append([wws, wls, lws, lls])
+                training_labels.append(1)
+            else:
+                training_data.append([lws, lls, wws, wls])
+                training_labels.append(0)
+    model = GaussianNB()
+    return model.fit(training_data, training_labels)
+    
 
 if __name__ == "__main__":
-    results_file, out_file = sys.argv[1:]
-    G, bowls = load_results_from_file(results_file)
-    teams = scored_teams(G)
-    for x in sorted(teams.values(), key=lambda x: x.combined_scores()):
-        print x.name, x.combined_scores()
+    results_dir, out_file = sys.argv[1:]
+    years = sorted(os.listdir(results_dir))
+    CURRENT_YEAR = years[-1]
+    bowls_by_year = {}
+    teams_by_year = {}
+    for year in years:
+        results_file = os.path.join(results_dir, year, "results.csv")
+        G, bowls = load_results_from_file(results_file)
+        teams = scored_teams(G)
+        bowls_by_year[year] = bowls
+        teams_by_year[year] = teams
+    model = train(teams_by_year, bowls_by_year)
+    for x in sorted(teams_by_year[CURRENT_YEAR].values(), key=lambda x: x.combined_scores()):
+        print(x.name, x.combined_scores())
     with open(out_file, 'w') as out:
-        for result in sorted(decide(bowls, teams)):
+        for result in sorted(decide(bowls_by_year[CURRENT_YEAR], teams_by_year[CURRENT_YEAR], model)):
             out.write("{},{},{}\n".format(*result))
-        
-    
-    
+
